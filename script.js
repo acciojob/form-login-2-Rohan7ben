2293ee8d65b3fb2ec6c2d401929845d3b2e7bc4c
Rohan7ben <112016528+Rohan7ben@users.noreply.github.com>
2024-01-08 19:10:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function myClick() {
+	const fName=document.getElementById("fn").value;
+	const lName=document.getElementById("ln").value;
+	const pNum=document.getElementById("ph").value;
+	const eMail=document.getElementById("em").value;
+	alert(`First Name:${fName}\nLast Name:${lName}\nPhone Number:${pNum}\nEmail ID:${eMail}`);
+	
+	
+
+
+	
+}
